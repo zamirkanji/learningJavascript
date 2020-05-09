4bf9f40bf52e096cd430ca85a650c27dd0255ec2
Zamir Kanji <zamirkanji@comcast.net>
2020-05-09 13:17:24 -0700

    learning bind() and finishing up functions - update 1

--- JSAdvancedConcepts/advancedFunctions/tcgFunctions/func.js
+++ JSAdvancedConcepts/advancedFunctions/tcgFunctions/func.js
@@ -24,10 +24,10 @@ console.dir(startGame); // function is an object (special type with preconfigure
 
 
 //passing a reference to a function (startGame() or anonymous func below to another function: addEvenetListener())
-startGameBtn.addEventListener("click", startGame);
-startGameBtn.addEventListener("click", function () { //anonymous function
-    console.log("Game is starting...");
-}); //addeventlistener method attached to object
+// startGameBtn.addEventListener("click", startGame);
+// startGameBtn.addEventListener("click", function () { //anonymous function
+//     console.log("Game is starting...");
+// }); //addeventlistener method attached to object
 
 
 
@@ -40,3 +40,99 @@ const add = function (a, b) {
     return a + b;
 }
 
+
+//DEFAULT ARGUMENTS
+const defaultEx = (a = 10) => console.log(a);
+
+defaultEx(); //returns 10 if no argument is passed through as a parameter
+
+//REST parameter
+
+//could have an array and add them all up with a for of loop
+const sumUp = (numbers) => {
+    let sum = 0;
+    for (const num of numbers) {
+        sum += num
+    }
+    return sum;
+}
+
+// sumUp(1, 5, 10, 15, 30, -6);
+console.log(sumUp([1, 5, 8, 69])); // 83
+
+
+//rest operator - builds an array inside the function with all of the arguments called into the function 
+const sumUp2 = (a, ...numbers) => {
+    let sum = 0;
+    for (const num of numbers) {
+        sum += num
+    }
+    return sum;
+}
+
+console.log(12, sumUp2(1, 8, 4, 29)); //42
+// console.log(subtractUp(420, 69));
+
+
+
+
+
+
+
+
+
+
+
+//function keyword function - has a "arguments" variable 
+//takes all arguments - same thing as rest operator 
+
+// const subtractUp = function (resultHandler, ...numbers) {
+//     let sum = 0;
+//     // for (const num of arguments) { //dont use this keyword
+//     //     sum += num
+//     // }
+//     for (const num of numbers) { //dont use this keyword
+//         sum -= num;
+//     }
+
+//     // return sum;
+//     resultHandler(sum);
+// }
+
+
+
+
+//example - function inside function/BIND()
+const funcInside = (resultHandler, operation, ...numbers) => {
+    const validateNumber = (number) => {
+        return isNaN(number) ? 0 : number;
+    }
+    let sum = 0;
+    numbers.map(num => {
+        if (operation === "ADD") {
+            sum += validateNumber(num); //is "num" zero? otherwise keep it 
+        } else {
+            sum -= validateNumber(num);
+        }
+    })
+    // return sum;
+    resultHandler(sum);
+}
+
+
+//callback func
+const showResult = (result, messageText) => {
+    console.log(messageText + ` ${result}`)
+};
+
+
+//outputs
+console.log(funcInside(showResult.bind(this, "The result after adding all numbers is"), "ADD", 3, 9, -7, 0, "hehe")); // 5 - no error
+//showResult is "resultHandler" argument - as a callback function
+
+// console.log(subtractUp(showResult, 19, 34, 984)); // still says "adding all numbers" (instead of subtracting)
+
+console.log(funcInside(showResult.bind(this, "The result after subtracting all numbers is"), "SUBTRACT", 19, 34, 984)); // still says "adding all numbers" (instead of subtracting)
+
+
+//callback function is called for you by something else - cant control when its called - you dont know when the click occures (addEventListener("click"))
