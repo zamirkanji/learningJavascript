735dcc82fcff048369c03bccf5ba12a07d0eae3d
Zamir Kanji <zamirkanji@comcast.net>
2020-08-17 10:10:43 -0700

    update 8/17/20

--- JSAdvancedConcepts/JSAdvancedPractice/advancedZtmQs/divide.js
+++ JSAdvancedConcepts/JSAdvancedPractice/advancedZtmQs/divide.js
@@ -2,26 +2,30 @@ const tempArr = [];
 const finalDivide = [];
 
 export const divideArray = (arr) => {
-    const sorted = arr.sort(function (a, b) {
-        return a - b;
-    });
+  const sorted = arr.sort(function (a, b) {
+    return a - b;
+  });
 
-    for (let i = 0; i < sorted.length; i++) {
-        if (sorted[i] == sorted[i + 1] && sorted[i] == sorted[i - 1]) {
-            tempArr.push(sorted[i]);
-        }
-        // let i = 0;
-        // // while (i < tempArr.length; i++) {
-        // while (sorted[i] == tempArr[i]) {
-        // 	tempArr.push(sorted[i]);
-        // 	i++;
-        // }
-        // }
+  //   const mySet = new Set(sorted);
+  //   console.log(mySet);
+
+  for (let i = 0; i < sorted.length; i++) {
+    if (sorted[i] == sorted[i + 1] && sorted[i] == sorted[i - 1]) {
+      tempArr.push(sorted[i]);
     }
+    // let i = 0;
+    // // while (i < tempArr.length; i++) {
+    // while (sorted[i] == tempArr[i]) {
+    // 	tempArr.push(sorted[i]);
+    // 	i++;
+    // }
+    // }
+  }
 
-    console.log(tempArr, "temp");
-    console.log(sorted, "sorted");
-    output.textContent = sorted;
+  console.log(tempArr, "temp");
+  console.log(sorted, "sorted");
+  output.textContent = sorted;
+  output2.textContent = tempArr;
 };
 
 // divideArray([1, 2, 4, 591, 392, 391, 2, 5, 10, 2, 1, 1, 1, 20, 20]);
